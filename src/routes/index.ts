6a3346144d8f74ac69a4a32d4329538cc48baacb
Ben <ben.armstrong22@gmail.com>
2023-07-27 18:59:31 +0800

    Don't version the /me route because I like how clean it is without versioning lol

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,8 @@
 import { route as meRoute } from './me.route'
 
-const base_path = 'v1'
-
 export const defaultRoutes = [
   {
-    path: `/${base_path}/me`,
+    path: '/me',
     route: meRoute
   }
 ]
