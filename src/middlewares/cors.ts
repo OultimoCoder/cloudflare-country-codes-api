8ac57235a3f1c79a157d37a660be019fc1a0cfa0
Ben <ben.armstrong22@gmail.com>
2023-07-27 19:27:16 +0800

    Await cors middleware call

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -4,5 +4,5 @@ import { getConfig } from '../config/config'
 
 export const setCors: MiddlewareHandler<Environment> = async (c, next) => {
   const config = getConfig(c.env)
-  cors({ origin: config.cors.origins, allowMethods: ['GET', 'OPTIONS'] })(c, next)
+  await cors({ origin: config.cors.origins, allowMethods: ['GET', 'OPTIONS'] })(c, next)
 }
