c1c81b87704021fd6c6e6735ee0277087bcf669a
Ben <ben.armstrong22@gmail.com>
2023-07-27 17:36:56 +0800

    Remove sentry error handling

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,11 +1,8 @@
-import { getSentry } from '@hono/sentry'
 import type { ErrorHandler } from 'hono'
 import { StatusCode } from 'hono/utils/http-status'
 import httpStatus from 'http-status'
-import type { Toucan } from 'toucan-js'
 import { ZodError } from 'zod'
 import { generateErrorMessage, ErrorMessageOptions } from 'zod-error'
-import { Environment } from '../../bindings'
 import { getConfig } from '../config/config'
 import { ApiError } from '../utils/ApiError'
 
@@ -13,7 +10,7 @@ const zodErrorOptions: ErrorMessageOptions = {
   transform: ({ errorMessage, index }) => `Error #${index + 1}: ${errorMessage}`
 }
 
-export const errorConverter = (err: unknown, sentry: Toucan): ApiError => {
+export const errorConverter = (err: unknown): ApiError => {
   let error = err
   if (error instanceof ZodError) {
     const errorMessage = generateErrorMessage(error.issues, zodErrorOptions)
@@ -25,10 +22,6 @@ export const errorConverter = (err: unknown, sentry: Toucan): ApiError => {
         ? castedErr.statusCode
         : httpStatus.INTERNAL_SERVER_ERROR
     const message = (castedErr.description || castedErr.message || httpStatus[statusCode]) as string
-    if (statusCode >= 500) {
-      // Log any unhandled application error
-      sentry.captureException(error)
-    }
     error = new ApiError(statusCode, message, false)
   }
   return error as ApiError
@@ -36,8 +29,7 @@ export const errorConverter = (err: unknown, sentry: Toucan): ApiError => {
 
 export const errorHandler: ErrorHandler<Environment> = (err, c) => {
   const config = getConfig(c.env)
-  const sentry = getSentry(c)
-  const error = errorConverter(err, sentry)
+  const error = errorConverter(err)
   if (config.env === 'production' && !error.isOperational) {
     error.statusCode = httpStatus.INTERNAL_SERVER_ERROR
     error.message = httpStatus[httpStatus.INTERNAL_SERVER_ERROR].toString()
@@ -48,6 +40,5 @@ export const errorHandler: ErrorHandler<Environment> = (err, c) => {
     message: error.message,
     ...(config.env === 'development' && { stack: err.stack })
   }
-  delete c.error // Don't pass to sentry middleware as it is either logged or already handled
   return c.json(response, error.statusCode as StatusCode)
 }
