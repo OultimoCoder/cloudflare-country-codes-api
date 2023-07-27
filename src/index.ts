2cce93b1b602b814b384a161d295bc1a4e34aee2
Ben <ben.armstrong22@gmail.com>
2023-07-27 18:36:42 +0800

    Prettify json response

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { prettyJSON } from 'hono/pretty-json'
 import httpStatus from 'http-status'
 import { errorHandler } from './middlewares/error'
 import { defaultRoutes } from './routes'
@@ -7,6 +8,7 @@ import { ApiError } from './utils/ApiError'
 
 const app = new Hono<Environment>()
 
+app.use('*', prettyJSON({ space: 4 }))
 app.use('*', cors())
 
 app.notFound(() => {
